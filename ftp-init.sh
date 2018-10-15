527a571b11226854b01dc2db9d8135ef3dfac3ef
GeekTR <geektheripper@gmail.com>
2018-10-15 16:33:50 +0800

    fix uid and username

--- ftp-init.sh
+++ ftp-init.sh
@@ -3,7 +3,7 @@
 docker-compose exec ftp bash
 
 pure-pw useradd root    -f "$FTP_PASSWD_DB" -m -u "$FTP_USER_UID" -g "$FTP_USER_GID" -d "$FTP_USER_HOME"
-pure-pw useradd guest   -f "$FTP_PASSWD_DB" -m -u 33              -g 33              -d "$FTP_USER_HOME/files"
+pure-pw useradd guest   -f "$FTP_PASSWD_DB" -m -u 9999            -g 9999            -d "$FTP_USER_HOME/files"
 pure-pw useradd manager -f "$FTP_PASSWD_DB" -m -u "$FTP_USER_UID" -g "$FTP_USER_GID" -d "$FTP_USER_HOME/files"
-pure-pw useradd bloger  -f "$FTP_PASSWD_DB" -m -u "$FTP_USER_UID" -g "$FTP_USER_GID" -d "$FTP_USER_HOME/blog"
+pure-pw useradd blogger -f "$FTP_PASSWD_DB" -m -u "$FTP_USER_UID" -g "$FTP_USER_GID" -d "$FTP_USER_HOME/blog"
 pure-pw useradd geektr  -f "$FTP_PASSWD_DB" -m -u "$FTP_USER_UID" -g "$FTP_USER_GID" -d "$FTP_USER_HOME"
