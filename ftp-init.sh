158205ebd9064f94bf3271e42ddfa283e347ae4e
GeekTR <geektheripper@gmail.com>
2018-10-24 22:59:22 +0800

    make script more robust

--- ftp-init.sh
+++ ftp-init.sh
@@ -3,7 +3,8 @@
 docker-compose exec ftp bash
 
 pure-pw useradd root    -f "$FTP_PASSWD_DB" -m -u "$FTP_USER_UID" -g "$FTP_USER_GID" -d "$FTP_USER_HOME"
-pure-pw useradd guest   -f "$FTP_PASSWD_DB" -m -u 9999            -g 9999            -d "$FTP_USER_HOME/files"
+useradd -G ftpgroup -s /sbin/nologin ftp-readonly
+pure-pw useradd guest   -f "$FTP_PASSWD_DB" -m -u ftp-readonly    -g ftp-readonly    -d "$FTP_USER_HOME/files"
 pure-pw useradd manager -f "$FTP_PASSWD_DB" -m -u "$FTP_USER_UID" -g "$FTP_USER_GID" -d "$FTP_USER_HOME/files"
 pure-pw useradd blogger -f "$FTP_PASSWD_DB" -m -u "$FTP_USER_UID" -g "$FTP_USER_GID" -d "$FTP_USER_HOME/blog"
 pure-pw useradd geektr  -f "$FTP_PASSWD_DB" -m -u "$FTP_USER_UID" -g "$FTP_USER_GID" -d "$FTP_USER_HOME"
